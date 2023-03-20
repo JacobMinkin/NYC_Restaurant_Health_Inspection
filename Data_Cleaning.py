4088dc701a8a1a581be1b9de048d5804dd9aa593
JacobMinkin <Jakemanthebakeman@gmail.com>
2023-03-20 14:20:27 -0400

    New File that is cleaner

---
+++ Data_Cleaning.py
@@ -0,0 +1,22 @@
+# Import Libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import geopandas as gpd
+
+#Import Data
+df = pd.read_csv('../data/Inspection.csv')
+df.set_index('CAMIS')
+#Change Name of Column for easier searching
+df.rename(columns = {'VIOLATION CODE':'CODE'}, inplace = True)
+
+# Convert date into pandas date/time format
+df['date'] = pd.to_datetime(df['INSPECTION DATE'])
+
+#Drop Columns: most of these are empty cols to beging with
+df = df.drop(['Zip Codes', 'City Council Districts', 'Police Precincts', 'Location Point', 'Community Districts', 
+   'Borough Boundaries',  'GRADE DATE', 'PHONE', 'INSPECTION DATE'], axis = 1)
+
+to_convert = []
+df[to_convert].astype('category')
