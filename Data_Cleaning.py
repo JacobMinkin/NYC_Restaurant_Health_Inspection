3d659874d449b597cf30b245d9d942fb577a7fb1
JacobMinkin <Jakemanthebakeman@gmail.com>
2023-03-23 02:52:51 -0400

    updated

--- Data_Cleaning.py
+++ Data_Cleaning.py
@@ -32,6 +32,28 @@ for name in Name_list:
     Name_dicts[name] = df.DBA[df.CAMIS == name].unique()
     latest_inspection_dicts[name] = df[df.CAMIS == name].date.max()
 
+#Imputing Time: I had to seperate the imputations for better results
+impute_variables_1 = ['CAMIS', 'CBoard', 'Longitude', 'Latitude']
+impute_variables_2 = ['CAMIS', 'ZIPCODE', 'Longitude', 'Latitude']
+
+impute_df_1 = df[impute_variables_1].groupby('CAMIS').first()
+impute_df_2 = df[impute_variables_2].groupby('CAMIS').first()
+
+imputer1 = KNNImputer(n_neighbors=2, weights="uniform")
+imputer2 = KNNImputer(n_neighbors=2, weights="uniform")
+a_1=imputer1.fit_transform(impute_df_1)
+a_2=imputer2.fit_transform(impute_df_2)
+
+df_imputed_1 = pd.DataFrame(a_1, columns= list(impute_df_1), index= impute_df_1.index)
+df_imputed_2 = pd.DataFrame(a_2, columns= list(impute_df_2), index= impute_df_2.index)
+Community_Board_Imputed = {}
+ZIPCODE_Imputed = {}
+for name in Name_list:
+    Community_Board_Imputed[name] = df_imputed_1.CBoard[name]
+    ZIPCODE_Imputed[name] = df_imputed_2.ZIPCODE[name]
+df['CBoard'] = df.CAMIS.map(Community_Board_Imputed)
+df['ZIPCODE'] = df.CAMIS.map(ZIPCODE_Imputed)
+
 # Making new variables first is Latest inspection date and second is a dummy variable that keeps True if it is the latest inspection
 df['Latest_Inspection'] = df.CAMIS.map(latest_inspection_dicts)
 df['isLatest'] = pd.to_datetime(df['INSPECTION DATE']) == df.Latest_Inspection
@@ -56,17 +78,3 @@ final_df[to_convert] = final_df[to_convert].astype('category')
 final_df = final_df.drop(final_df[final_df.date== '1900-01-01'].index)
 final_df = final_df[finalVariables]
 
-#Imputing Time: I might need to move the order around soon
-impute_variables_1 = ['CAMIS', 'CBoard', 'Longitude', 'Latitude']
-impute_variables_2 = ['CAMIS', 'ZIPCODE', 'Longitude', 'Latitude']
-
-impute_df = df[impute_variables_1].groupby('CAMIS').first()
-
-imputer = KNNImputer(n_neighbors=2, weights="uniform")
-a=imputer.fit_transform(impute_df)
-
-df_imputed = pd.DataFrame(a, columns= list(impute_df), index= impute_df.index)
-Community_Board_Imputed = {}
-for name in Name_list:
-    Community_Board_Imputed[name] = df_imputed.CBoard[name]
-df['CBoard'] = df.CAMIS.map(Community_Board_Imputed)
